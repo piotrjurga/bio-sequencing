df4f1b7936fac3e35015159c3f9ae5fd2c4fdc2f
Piotr Jurga <benny.profane37@gmail.com>
2020-06-30 00:05:54 +0200

    graph optimization

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
-#define POPULATION  1024
-#define GENERATIONS 128
-#define MUTATIONS 2
+// fast
+#if 0
+#define POPULATION  512
+#define GENERATIONS 8
+#define PARENTS     (POPULATION/2)
+//#define BREED
+#define SPARSE_GRAPH
+#endif
+
+// normal
+#if 1
+#define POPULATION  (1024*2)
+#define GENERATIONS (1024*2)
+#define PARENTS     (POPULATION/2)
 #define BREED
 #define SPARSE_GRAPH
+#endif
+
+#define MUTATIONS   2
 #define PARALLEL
 //#define SINGLE_TEST
+
+// for the moment optimizing the graph actually makes the results worse
+// it may be caused by storing the merged nodes in candidates
+//#define OPTIMIZE_GRAPH
+
+// hypothetical alternate breeding schemes:
+// find a place in the parent_a where it would be appropriate
+// to try something else and only then switch to parent_b
+// - probably optimize and score at the same time, cuz it's easy
+// - and mutate at the same time too?
+// - could try some heuristic like split before a costly edge
+// - could find the split at random - better diversity
+// - could litterally dfs the crap out of the parents combined graph
+
 
 #include <stdio.h>
 #include <stdint.h>
@@ -20,6 +48,10 @@
 #define STB_DEFINE
 #define STB_NO_REGISTRY
 #include "stb.h"
+
+// platform independent high precision timer
+#define SOKOL_IMPL
+#include "sokol_time.h"
 
 typedef int8_t  s8;
 typedef int16_t s16;
@@ -157,6 +189,74 @@ s32 optimize_and_score(Edge *candidate, Node *graph, s32 onct_length,
     return oncts_visited;
 }
 
+void optimize_graph(Node *graph, s32 node_count) {
+    s32 total_optimized = 0;
+    // find optimal edges connecting [i] to [j]
+    s32 optimal_edges[1024][1024] = {};
+    for (s32 node_i = 0; node_i < node_count; node_i++) {
+        Node node = graph[node_i];
+        for (s32 edge_i = 0; edge_i < node.edge_count; edge_i++) {
+            Edge e = node.edges[edge_i];
+            if (e.cost != 1) break;
+            optimal_edges[node_i][e.next] += 1;
+        }
+    }
+#if 0
+    for (s32 i = 0; i < node_count; i++) {
+        for (s32 j = 0; j < node_count; j++) {
+            printf("%d;", optimal_edges[i][j]);
+        }
+        puts("");
+    }
+#endif
+    // search for nodes to merge
+    for (s32 node_i = 0; node_i < node_count; node_i++) {
+        s32 row_sum = 0;
+        s32 dest_j = -1;
+        for (s32 j = 0; j < node_count; j++) {
+            row_sum += optimal_edges[node_i][j];
+            if (optimal_edges[node_i][j] > 0) {
+                dest_j = j;
+            }
+            if (row_sum > 1) break;
+        }
+        if (row_sum != 1) continue;
+        s32 col_sum = 0;
+        for (s32 i = 0; i < node_count; i++) {
+            col_sum += optimal_edges[i][dest_j];
+            if (col_sum > 1) break;
+        }
+        if (col_sum != 1) continue;
+        // at this point we know the nodes can be merged
+        total_optimized++;
+        // delete all suboptimal edges from source node
+        {
+            Node *node = &graph[node_i];
+            node->edges[0].next = dest_j;
+            node->edges[0].cost = 1;
+            node->edge_count = 1;
+        }
+
+        // delete all suboptimal edges into dest node
+        for (s32 i = 0; i < node_count; i++) {
+            Node *node = &graph[i];
+            if (i == node_i || i == dest_j) continue;
+            for (s32 edge_i = 0; edge_i < node->edge_count; edge_i++) {
+                if (node->edges[edge_i].next != dest_j) continue;
+
+                s32 remaining_edges = node->edge_count - edge_i - 1;
+                memmove(&node->edges[edge_i],
+                        &node->edges[edge_i+1],
+                        remaining_edges * sizeof(Edge));
+                node->edge_count--;
+                break;
+            }
+        }
+    }
+
+    printf("merged nodes %d times\n", total_optimized);
+}
+
 Edge * solve(char **dict, s32 dict_size, s32 original_oncts, double *percent_score) {
     s32 onct_length = strlen(dict[0]);
     s32 max_solution_length = original_oncts + onct_length - 1;
@@ -203,6 +303,10 @@ Edge * solve(char **dict, s32 dict_size, s32 original_oncts, double *percent_sco
         total_edges += graph[node_i].edge_count;
     }
 
+#ifdef OPTIMIZE_GRAPH
+    optimize_graph(graph, node_count);
+#endif
+
 #if 0
     puts("GRAPH:");
     for (s32 i = 0; i < node_count; i++) {
@@ -220,7 +324,7 @@ Edge * solve(char **dict, s32 dict_size, s32 original_oncts, double *percent_sco
     //
 
     s32 population = POPULATION;
-    s32 parent_count = population / 10;
+    s32 parent_count = PARENTS;
     s32 candidate_size = node_count * sizeof(Edge);
     u8 *candidates = (u8 *)calloc(population + parent_count, candidate_size);
     u8 *parents = candidates + population * candidate_size;
@@ -275,61 +379,68 @@ Edge * solve(char **dict, s32 dict_size, s32 original_oncts, double *percent_sco
 
         qsort(scores, population, sizeof(Score), stb_intcmprev(0));
 
-        // save the best solutions for breeding
-        // TODO(piotr): parallel for?
-        for (s32 parent_i = 0; parent_i < parent_count; parent_i++) {
-            Edge *parent_slot = (Edge *)(parents + parent_i*candidate_size);
-            s32 old_index = scores[parent_i].index;
-            scores[parent_i].index = parent_i;
-            Edge *parent = (Edge *)(candidates + old_index*candidate_size);
-            memcpy(parent_slot, parent, candidate_size);
-        }
-        // move them to the beggining of the population
-        memcpy(candidates, parents, parent_count * candidate_size);
-
-        // set the rest of the population to modified versions of parents
 #ifdef PARALLEL
-#pragma omp parallel for
+#pragma omp parallel
 #endif
-        for (s32 candidate_index = parent_count;
-             candidate_index < population;
-             candidate_index++)
         {
-#ifdef BREED
-            s32 parent_a_i = candidate_index % parent_count;
-            s32 parent_b_i = stb_rand() % parent_count;
-            s32 split = stb_rand() % node_count;
-            //s32 split = node_count/2;
-            s32 size_a = split * sizeof(Edge);
-            //s32 size_a = (node_count/2) * sizeof(Edge);
-            s32 size_b = candidate_size - size_a;
-            Edge *parent_a = (Edge *)(candidates + parent_a_i*candidate_size);
-            Edge *parent_b = (Edge *)(candidates + parent_b_i*candidate_size + size_a);
-            Edge *candidate   = (Edge *)(candidates + candidate_index*candidate_size);
-            Edge *candidate_b = (Edge *)(candidates + candidate_index*candidate_size + size_a);
-            // move the first half of the genes from the first parent
-            memcpy(candidate, parent_a, size_a);
-            // move the second half of the genes from the second parent
-            memcpy(candidate_b, parent_b, size_b);
-#else
-            Edge *candidate = (Edge *)(candidates + candidate_index*candidate_size);
-            s32 parent_i = candidate_index % parent_count;
-            Edge *parent = (Edge *)(candidates + parent_i*candidate_size);
-            memcpy(candidate, parent, candidate_size);
+            // save the best solutions for breeding
+#ifdef PARALLEL
+#pragma omp for
 #endif
-
-            // mutate
-            for (s32 i = 0; i < MUTATIONS; i++) {
-                s32 node_to_mutate = stb_rand() % node_count;
-                Node node = graph[node_to_mutate];
-                double rand_v = stb_frand();
-                s32 new_edge = (s32)(rand_v * rand_v * node.edge_count);
-                candidate[node_to_mutate] = node.edges[new_edge];
+            for (s32 parent_i = 0; parent_i < parent_count; parent_i++) {
+                Edge *parent_slot = (Edge *)(parents + parent_i*candidate_size);
+                s32 old_index = scores[parent_i].index;
+                scores[parent_i].index = parent_i;
+                Edge *parent = (Edge *)(candidates + old_index*candidate_size);
+                memcpy(parent_slot, parent, candidate_size);
             }
-            s32 score = optimize_and_score(candidate, graph, onct_length,
-                                           max_solution_length, node_count);
-            scores[candidate_index].oncts = score;
-            scores[candidate_index].index = candidate_index;
+            // move them to the beggining of the population
+#pragma omp single
+            memcpy(candidates, parents, parent_count * candidate_size);
+
+            // set the rest of the population to modified versions of parents
+#ifdef PARALLEL
+#pragma omp for
+#endif
+            for (s32 candidate_index = parent_count;
+                    candidate_index < population;
+                    candidate_index++)
+            {
+#ifdef BREED
+                s32 parent_a_i = candidate_index % parent_count;
+                s32 parent_b_i = stb_rand() % parent_count;
+                s32 split = stb_rand() % node_count;
+                //s32 split = node_count/2;
+                s32 size_a = split * sizeof(Edge);
+                //s32 size_a = (node_count/2) * sizeof(Edge);
+                s32 size_b = candidate_size - size_a;
+                Edge *parent_a = (Edge *)(candidates + parent_a_i*candidate_size);
+                Edge *parent_b = (Edge *)(candidates + parent_b_i*candidate_size + size_a);
+                Edge *candidate   = (Edge *)(candidates + candidate_index*candidate_size);
+                Edge *candidate_b = (Edge *)(candidates + candidate_index*candidate_size + size_a);
+                // move the first half of the genes from the first parent
+                memcpy(candidate, parent_a, size_a);
+                // move the second half of the genes from the second parent
+                memcpy(candidate_b, parent_b, size_b);
+#else
+                Edge *candidate = (Edge *)(candidates + candidate_index*candidate_size);
+                s32 parent_i = candidate_index % parent_count;
+                Edge *parent = (Edge *)(candidates + parent_i*candidate_size);
+                memcpy(candidate, parent, candidate_size);
+#endif
+                // mutate
+                for (s32 i = 0; i < MUTATIONS; i++) {
+                    s32 node_to_mutate = stb_rand() % node_count;
+                    Node node = graph[node_to_mutate];
+                    double rand_v = stb_frand();
+                    s32 new_edge = (s32)(rand_v * rand_v * node.edge_count);
+                    candidate[node_to_mutate] = node.edges[new_edge];
+                }
+                s32 score = optimize_and_score(candidate, graph, onct_length,
+                        max_solution_length, node_count);
+                scores[candidate_index].oncts = score;
+                scores[candidate_index].index = candidate_index;
+            }
         }
     }
 
@@ -352,19 +463,22 @@ Edge * solve(char **dict, s32 dict_size, s32 original_oncts, double *percent_sco
 
 int main(int argc, char **argv) {
     stb_srand(time(0));
+    stm_setup();
 
 #ifdef SINGLE_TEST
-    //char *path = "test.txt";
-    char *path = "Instances/RandomNegativeErrors/9.200-40.txt";
-    s32 original_oncts = 200;
+    char *path = "test.txt";
+    //char *path = "Instances/RandomNegativeErrors/9.200-40.txt";
+    s32 original_oncts = 10;
     char **dict;
     s32 dict_size;
     dict = stb_stringfile(path, &dict_size);
 
     double percent_score = 0;
+    u64 start_time = stm_now();
     Edge *best = solve(dict, dict_size, original_oncts, &percent_score);
     (void)best;
-    printf("9.200-40.txt\t%f%%\n", percent_score);
+    double elapsed = stm_ms(stm_since(start_time));
+    printf("9.200-40.txt\t%f%%\t%fms\n", percent_score, elapsed);
     //print_path(dict, best, 209);
     //print_solution(dict, best, 209);
 
@@ -380,6 +494,7 @@ int main(int argc, char **argv) {
     DIR *problem_dir = opendir(problem_dir_path);
 
     double *scores = 0;
+    double *times = 0;
 
     struct dirent *dir_entry;
     while (dir_entry = readdir(problem_dir)) {
@@ -398,20 +513,29 @@ int main(int argc, char **argv) {
         //s32 max_solution_length = original_oncts + onct_length - 1;
         double percent_score = 0;
 
+        u64 start_time = stm_now();
         Edge *best = solve(dict, dict_size, original_oncts, &percent_score);
         (void)best;
+        double elapsed = stm_ms(stm_since(start_time));
         stb_arr_push(scores, percent_score);
-        printf("%s\t%f%%\n", dir_entry->d_name, percent_score);
+        stb_arr_push(times, elapsed);
+        printf("%s\t%f%%\t%fms\n", dir_entry->d_name, percent_score, elapsed);
         //s32 result = score_candidate(best, onct_length, max_solution_length, dict_size);
     }
 
-    // calculate average percent score
-    double sum = 0;
+    // calculate average score and time
+    double sum_score = 0;
     for (s32 i = 0; i < stb_arr_len(scores); i++) {
-        sum += scores[i];
+        sum_score += scores[i];
     }
-    double average = sum / stb_arr_len(scores);
-    printf("average = %f%%\n", average);
+    double sum_time = 0;
+    for (s32 i = 0; i < stb_arr_len(times); i++) {
+        sum_time += times[i];
+    }
+    double average_score = sum_score / stb_arr_len(scores);
+    double average_time = sum_time / stb_arr_len(times);
+    puts("______________________________________________");
+    printf("average\t\t%f%%\t%fms\n", average_score, average_time);
         
     return 0;
 }
--- make.bat
+++ make.bat
@@ -7,7 +7,7 @@ where cl >nul 2>nul
 if %ERRORLEVEL% neq 0 call %VCVARSPATH%
 
 mkdir build >nul 2>nul
-cl /nologo /Zi /MT /O2 /Oi /openmp main.cpp
+cl /nologo /Zi /MT /O2 /Oi /F10485760 /openmp main.cpp
 move *.obj build >nul 2>nul
 move *.pdb build >nul 2>nul
 move *.ilk build >nul 2>nul
